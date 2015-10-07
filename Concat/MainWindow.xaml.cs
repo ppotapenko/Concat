0d33f9521a9e51d5b62a89e9bca4aaf208455584
potapenkopp@gmail.com <potapenkopp@gmail.com>
2015-10-07 15:42:40 +0300

    first commit

---
+++ Concat/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace Concat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _dirPath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSelectFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            CommonFileDialogResult result = dialog.ShowDialog();
+            if (result == CommonFileDialogResult.Ok)
+            {
+                TextBoxDirPath.Text = dialog.FileName;
+                _dirPath = dialog.FileName;
+            }
+        }
+
+        private void ButtonAddToIgnore_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            dialog.InitialDirectory = _dirPath;
+            CommonFileDialogResult result = dialog.ShowDialog();
+            if (result == CommonFileDialogResult.Ok)
+            {
+                TextBoxIgnoreFolders.Text += dialog.FileName.Remove(0, _dirPath.Length) + ";\n";
+            }
+        }
+    }
+}
