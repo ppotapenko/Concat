676444220bae408c7904991fbb748ceaf144fdf6
potapenkopp@gmail.com <potapenkopp@gmail.com>
2015-10-13 15:59:29 +0300

    refactoring

--- Concat/MainWindow.xaml.cs
+++ Concat/MainWindow.xaml.cs
@@ -4,7 +4,6 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
 using Microsoft.WindowsAPICodePack.Dialogs;
 
 #endregion
@@ -53,46 +52,66 @@ namespace Concat
 
         private void ButtonGetResult_Click(object sender, RoutedEventArgs e)
         {
-            var dialog = new CommonSaveFileDialog
+            var dirPath = TextBoxDirPath.Text;
+            if (string.IsNullOrWhiteSpace(dirPath))
             {
-                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
-                DefaultFileName = new DirectoryInfo(TextBoxDirPath.Text).Name,
-                AlwaysAppendDefaultExtension = true,
-                DefaultExtension = "txt"
-            };
-            dialog.AlwaysAppendDefaultExtension = true;
-            dialog.Filters.Add(CommonFileDialogStandardFilters.TextFiles);
-            var result = dialog.ShowDialog();
-            if (result == CommonFileDialogResult.Ok)
+                MessageBox.Show(string.Format("Поле директория не может быть пустым"));
+            }
+            else if (!Directory.Exists(dirPath))
             {
-                var ignoreFolders =
-                    TextBoxIgnoreFolders.Text
-                        .Trim()
-                        .Replace("\r", String.Empty)
-                        .Replace("\n", String.Empty)
-                        .Replace(" ", String.Empty)
-                        .TrimEnd(';')
-                        .Split(';')
-                        .ToList();
-
-                var globalIgnoreFolders =
-                    TextBoxGlobalIgnorFolders.Text
-                        .Trim()
-                        .Replace("\r", String.Empty)
-                        .Replace("\n", String.Empty)
-                        .Replace(" ", String.Empty)
-                        .TrimEnd(',')
-                        .Split(',')
-                        .ToList();
-
-                var fileCounter = new FileCounter(TextBoxDirPath.Text, TextBoxFilterExt.Text, ignoreFolders, globalIgnoreFolders);
-                var progressWindow = new ProgressWindow(fileCounter, dialog.FileName, TextBoxFileTitle.Text)
+                MessageBox.Show(string.Format("Директория не существует! Проверте указанный путь: \"{0}\"", dirPath));
+            }
+            else
+            {
+                try
                 {
-                    ButtonStart = {Visibility = Visibility.Hidden}
-                };
+                    var dialog = new CommonSaveFileDialog
+                    {
+                        InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
+                        DefaultFileName = new DirectoryInfo(dirPath).Name,
+                        AlwaysAppendDefaultExtension = true,
+                        DefaultExtension = "txt"
+                    };
+                    dialog.AlwaysAppendDefaultExtension = true;
+                    dialog.Filters.Add(CommonFileDialogStandardFilters.TextFiles);
+                    var result = dialog.ShowDialog();
+                    if (result == CommonFileDialogResult.Ok)
+                    {
+                        var ignoreFolders =
+                            TextBoxIgnoreFolders.Text
+                                .Trim()
+                                .Replace("\r", String.Empty)
+                                .Replace("\n", String.Empty)
+                                .Replace(" ", String.Empty)
+                                .TrimEnd(';')
+                                .Split(';')
+                                .ToList();
 
-                //runs the progress operation upon window load
-                progressWindow.ShowDialog();
+                        var globalIgnoreFolders =
+                            TextBoxGlobalIgnorFolders.Text
+                                .Trim()
+                                .Replace("\r", String.Empty)
+                                .Replace("\n", String.Empty)
+                                .Replace(" ", String.Empty)
+                                .TrimEnd(',')
+                                .Split(',')
+                                .ToList();
+
+                        var fileCounter = new FileCounter(dirPath, TextBoxFilterExt.Text, ignoreFolders,
+                            globalIgnoreFolders);
+                        var progressWindow = new ProgressWindow(fileCounter, dialog.FileName, TextBoxFileTitle.Text)
+                        {
+                            ButtonStart = {Visibility = Visibility.Hidden}
+                        };
+
+                        //runs the progress operation upon window load
+                        progressWindow.ShowDialog();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(string.Format("Произошла ошибка!\r\n{0}", ex.Message));
+                }
             }
         }
     }
